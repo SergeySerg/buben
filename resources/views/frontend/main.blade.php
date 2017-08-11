85f487400684210e5b9f742ada64dd66bc5f0410
Andrii Melnichuk <andrewlutsk88@gmail.com>
2017-08-11 23:42:52 +0300

    fix menu

--- resources/views/frontend/main.blade.php
+++ resources/views/frontend/main.blade.php
@@ -25,18 +25,6 @@
     </div>
     <div class="nav-container">
         <div class="via-1490875280654" via="via-1490875280654" vio="111">
-            <div class="bar bar--sm hidden">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xs-3 col-sm-2">
-                            <a href="/{{ App::getLocale() }}"> <img class="logo logo-dark" alt="logo" src="{{ asset('/img/frontend/logo.png') }}"> <img class="logo logo-light" alt="logo" src="{{ asset('/img/frontend/logo-light.png') }}"> </a>
-                        </div>
-                        <div class="col-xs-9 col-sm-10 text-right">
-                            <a href="#" class="hamburger-toggle" data-toggle-class="#menu1;hidden-xs hidden-sm"> <i class="icon icon--sm stack-interface stack-menu"></i> </a>
-                        </div>
-                    </div>
-                </div>
-            </div>
             <nav id="menu1" class="bar bar-1 r-bar">
                 <div class="container">
                     <div class="row">
